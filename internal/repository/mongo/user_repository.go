3d23d95b5c026daca70d43230f3e0c03eb30ca3e
juven0 <bocquelin@gmail.com>
2025-01-10 15:23:04 +0300

    create collection

--- internal/repository/mongo/user_repository.go
+++ internal/repository/mongo/user_repository.go
@@ -1,10 +1,21 @@
 package mongo
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 var userCollection *mongo.Collection
+var ctx = context.TODO()
 
 func InitUserRepository(db *mongo.Database){
 	userCollection = db.Collection("user")
+}
+
+func GetAllUser (){
+	filter := bson.D{}
+	userCollection.Find(ctx, filter)
 }
 
