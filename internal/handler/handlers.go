b2bf49e967bdf4408e62e89bdd8d7d08906db8e0
juven0 <bocquelin@gmail.com>
2025-01-14 14:46:23 +0300

    user handler done

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -35,3 +35,28 @@ func GetOneUserHandler(c *fiber.Ctx)error{
 	}
 	return c.Status(200).JSON(fiber.Map{"user": userFund})
 }
+
+func UpdateUserHandler(c *fiber.Ctx)error{
+	id:= c.Params("id")
+	user := &mongomodels.User{}
+
+	if err := c.BodyParser(user); err != nil{
+		return c.Status(500).JSON(fiber.Map{"error": err})
+	}
+	result, err := mongoservice.UpdateUser(id, user)
+	if err!= nil{
+		return c.Status(500).JSON(fiber.Map{"error": err})
+	}
+
+	return c.Status(200).JSON(fiber.Map{"result":result})
+}
+
+func DeleteUserHanbler(c *fiber.Ctx)error{
+	id := c.Params("id")
+
+	result, err := mongoservice.DeleteUser(id)
+	if err!= nil{
+		return c.Status(500).JSON(fiber.Map{"error": err})
+	}
+	return c.Status(200).JSON(fiber.Map{"result":result})
+}
