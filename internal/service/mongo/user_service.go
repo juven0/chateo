00b61e142addce1720f6538eed14725647ddd9b0
juven0 <bocquelin@gmail.com>
2025-01-29 14:22:56 +0300

    refactoring

--- internal/service/mongo/user_service.go
+++ internal/service/mongo/user_service.go
@@ -20,34 +20,28 @@ var(
 
 func GetUserServiceInstace()*UserService{
 	once.Do(func(){
-		repository := mongorepository.InitUserRepository(mongoClient.Database(tableName))
-		userServiceInstance = &UserSevice{
+		repository := mongorepository.NewUserRepository(mongoClient.Database(tableName))
+		userServiceInstance = &UserService{
 			repo: repository,
 		}
 	})
 	return userServiceInstance
 }
 
-func setupUserService() {
-	mongorepository.InitUserRepository(mongoClient.Database(tableName))
-}
 
 func (r *UserService)CreatUser(user *mongomodels.User) error{
-	setupUserService()
+
 	return r.repo.InsertUser(user)
 }
 
 func (r *UserService)GetUser(id string)(mongomodels.User, error){
-	setupUserService()
 	return r.repo.GetUser(id)
 }
 
 func (r *UserService)UpdateUser(id string, user *mongomodels.User)(*mongo.UpdateResult, error){
-	setupUserService()
 	return r.repo.UpdateUser(id, user)
 }
 
 func (r *UserService)DeleteUser(id string)(*mongo.DeleteResult, error){
-	setupUserService()
 	return r.repo.DeleteUser(id)
 }
